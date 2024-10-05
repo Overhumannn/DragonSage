9b53e370a7b4babbc51e409ccd77b4e8cc8f4d67
malim_helper <alexander.matsukidis@gmail.com>
2024-10-06 00:21:41 +0300

    Update database host configuration and refactor Docker Compose for container networking

--- app/database/db.py
+++ app/database/db.py
@@ -1,28 +1,26 @@
+import time
 import psycopg2
 from config import settings  # Импорт настроек для подключения к базе данных
 import logging
-import time
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def get_db_connection(retry_attempts=5, retry_delay=5):
-    for attempt in range(retry_attempts):
+def get_db_connection(retries=5, delay=5):
+    connection = None
+    for attempt in range(retries):
         try:
             connection = psycopg2.connect(
                 host=settings.db_host,
                 port=settings.db_port,
                 user=settings.db_user,
                 password=settings.db_password,
-                dbname=settings.db_name,
-                connect_timeout=10  # Устанавливаем тайм-аут подключения (в секундах)
+                dbname=settings.db_name
             )
-            logger.info("Успешное подключение к базе данных.")
             return connection
         except psycopg2.OperationalError as e:
-            logger.error(f"Ошибка подключения к базе данных: {e}. Попытка {attempt + 1} из {retry_attempts}.")
-            if attempt < retry_attempts - 1:
-                time.sleep(retry_delay)
+            logger.error(f"Ошибка подключения к базе данных: {e}")
+            if attempt < retries - 1:
+                time.sleep(delay)
+                logger.info(f"Повторная попытка подключения ({attempt + 2} из {retries})...")
             else:
                 raise e
